760f78fb8450f3aba6890f1d7cbc462b271f03bb
gersonsouto <gersonsouto94@gmail.com>
2020-03-21 16:19:26 -0300

    filtro periodo 5 anos

--- historical_summary/historical_summary/run_pandas.py
+++ historical_summary/historical_summary/run_pandas.py
@@ -5,5 +5,7 @@ dataset = pd.read_csv(r'./db.csv', sep=',')
 size_dataset = (len(dataset))
 database_csv = dataset.head(size_dataset)
 print (database_csv)
-
-
+print('---------------Resumo de 5 Anos---------------')
+dataset_5_anos = dataset.loc[dataset['periodo']=='5 Anos']
+ascending_dataset = dataset_5_anos.sort_values(by='variacao', ascending=False)
+print (ascending_dataset)
